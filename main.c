358041c5761738dd9aa0c06cdd582a58c074545c
__.:Kuror0:.__ <limaalyson@hotmail.com>
2022-07-17 11:33:20 -0300

    ultima versão do Aly

--- main.c
+++ main.c
@@ -7,41 +7,134 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-void http_request(char *msg ){
-    printf("Eu respondo requests http");
-    return ;
+char *meu_concat(char *a, char*b){
+
+    char *c = malloc(sizeof(a)+sizeof(b));
+    int a_len = strlen(a);
+    int i ;
+    for(i=0; i< a_len ; i++)
+    {
+        c[i] = 0;
+        c[i]+=a[i];
+    }
+
+    for( i=0; i< strlen(b); i++)
+    {
+        c[i + a_len] = b[i];
+    }
+    printf("O C final e %s" , c);
+    return c;
+}
+char *mystrcat(char *a, char *b) {
+  char *p, *q, *rtn;
+  rtn = q = malloc(strlen(a) + strlen(b) + 1);
+  for (p = a; (*q = *p) != '\0'; ++p, ++q) {}
+  for (p = b; (*q = *p) != '\0'; ++p, ++q) {}
+  return rtn;
+}
+
+void http_request(char *msg , int client_sock ){
+
+    char response[] = "HTTP/1.1 200 OK\n" ;
+    char aux[500] = "teste//";
+    //response = response + "Content-Type: image/png\n\n" ;
+    strcat(response ,"Content-Type: image/png\n\n") ;
+
+    //char a = msg[0];
+    printf("response == %s\n" , msg );
+    //strcat(response , fopen(msg,"rb") ) ;
+    printf("Eu respondo requests http OU NAO\n");
+    send(client_sock , response , sizeof(response) , 0 );
+    fflush(stdout) ;
 }
 void connect_players(){
     printf("Eu conecto players ");
     return ;
 }
 void *handle_request(void *client_sock_tmp ){
+    printf("Entrou no handle\n");
     int client_sock = *((int*)client_sock_tmp);
     free(client_sock_tmp);
 
     char buff[BUFF_SIZE];
-    int x = recv(client_sock , buff ,sizeof( buff) , 0);
 
+    int x = recv(client_sock , buff ,sizeof( buff) , 0);
+    printf("Passou no recv\n");
+    printf("\na requisicao de entrada e :\n%s\n\n", buff);
+    char * token = strtok(buff, " ");
     //Define se é uma conexão Http ou de entrada na partida e chama a devida função
-    /*
-    if(http){
-        http_request();
-    }else if (to_connect_player){
+    printf("a requisicao de entrada e :\n%s\n", buff);
+    if(!strcmp(buff ,"GET") ){
+        char oi[300] = "C:\\Users\\limaa\\OneDrive\\Documentos\\CodeBlocks Projects\\Servidor-Multiplayer-Ap2\\Server" ;
+        token = strtok(NULL, " ") ;
+        strcat(oi ,token) ;
+        char a[] = "HTTP/1.1 200 OK\nContent-Type: image/png\n\n";
+        /*strcat(a , ");
+        strcat(a , ");*/
+
+        int c = getc;
+        FILE *file ;/*, *file2;
+        file2 = fopen("C:\\temporario_me_apague.txt","w");
+        fprintf(file2 ,"%s", a );
+        if (file2) {
+            while ((c = getc(file2)) != EOF)
+        putchar(c);
+        fclose(file2);
+        }*/
+        file = fopen(oi, "r");
+        int index = 0 ;
+        char output[2000] = {0x00};
+        if (file) {
+            while ((c = getc(file)) != EOF)
+        //putchar(c);
+        output[index++] = (char)putchar(c);
+        fclose(file);
+}       char d[1000] ;
+        sprintf(d , "%d" , c);
+        printf("Concatenou\n");
+
+        char *msg = meu_concat(&a,&output);
+
+        printf("minha msg %s" , msg );
+        /*strcat(a , (char )c);*/
+        //printf("ola %s\n" , c );
+
+        //printf("%s\n" , buff);
+        //http_request( token , client_sock );
+    }else if (buff == "MSG"){
         connect_players();
-    }else{
+    }/*else{
         return bad_request;
     }
     */
     //return ;
 }
-void start_server(int client , int x , char buff , int csize , int caddr , int server ){
+void start_server( ){
+        WSADATA wsa ;
+   WSAStartup(MAKEWORD(2, 0 ), &wsa);
+
+     struct sockaddr_in caddr ;
+     struct sockaddr_in saddr = {
+         .sin_family = AF_INET,
+         .sin_addr.s_addr = htonl(INADDR_ANY),
+         .sin_port = htons(PORT)
+     } ;
+
+    int server = socket(AF_INET, SOCK_STREAM,0) ;
+    int client , csize = sizeof caddr , x;
+
+    bind(server , (struct sockaddr* ) &saddr , sizeof(saddr) ) ;
+    listen(server ,5 );
+
     printf("[Server Ouvindo...]");
 
     while(1)
-    {
+    {   printf("Chegou no client\n");
         client = accept(server , (struct sockaddr* ) &caddr , &csize );
+        printf("Passou do client\n");
         //x = recv(client , buff ,sizeof buff , 0);
         if(client >= 0 ){
+                printf("Entrou no if\n");
             //Chama nova thread()
             pthread_t tdid;
             int *client_sock = malloc(sizeof(int));
@@ -59,23 +152,10 @@ void start_server(int client , int x , char buff , int csize , int caddr , int s
 }
 int main(){
     char buff[129];
-    WSADATA wsa ;
-   WSAStartup(MAKEWORD(2, 0 ), &wsa);
 
-     struct sockaddr_in caddr ;
-     struct sockaddr_in saddr = {
-         .sin_family = AF_INET,
-         .sin_addr.s_addr = htonl(INADDR_ANY),
-         .sin_port = htons(5000)
-     } ;
+    printf("Foi_Meu_FILHO_FOI_testeOoo!\n");
 
-    int server = socket(AF_INET, SOCK_STREAM,0) ;
-    int client , csize = sizeof caddr , x;
-
-    bind(server , (struct sockaddr* ) &saddr , sizeof saddr) ;
-    listen(server ,5 );
-
-    printf("Foi_Meu_FILHO_FOI_testeOoo!");
+    start_server();
 
     /*
     while(1){
